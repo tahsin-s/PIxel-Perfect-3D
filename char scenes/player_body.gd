25e77909c584f2a0c6a4603ca0c99fc71bfd34a1
Tahsin Sarker <36494345+tahsin-s@users.noreply.github.com>
2023-12-28 13:12:50 -0500

    Add moving player

---
+++ char scenes/player_body.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+const ACCELERATION = 100.0
+const FRICTION = 10.0
+const SPEED_SCALING = Vector3.ONE / Globals.scale
+const JUMP_VELOCITY = 20.0
+const MOVING_THRESH = 0.3
+
+# Player specific gravity value.
+var gravity = 50
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	velocity.x -= velocity.x * FRICTION * SPEED_SCALING.x * delta
+	velocity.z -= velocity.z * FRICTION * SPEED_SCALING.z * delta
+	if direction:
+		velocity.x += direction.x * ACCELERATION * delta * SPEED_SCALING.x
+		velocity.z += direction.z * ACCELERATION * delta * SPEED_SCALING.z
+	move_and_slide()
+	if velocity.length() <= MOVING_THRESH:
+		position = position.snapped(Globals.snap)
