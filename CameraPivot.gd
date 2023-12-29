6733d1bbdc4384ea28bb8796eff9080a0b6a627b
Tahsin Sarker <36494345+tahsin-s@users.noreply.github.com>
2023-12-28 20:59:08 -0500

    Move camera with player

---
+++ CameraPivot.gd
@@ -0,0 +1,11 @@
+extends Marker3D
+
+@export var target: Node3D
+@export var offset: Vector3;
+@onready var parent: Node3D = get_parent()
+# Called when the node enters the scene tree for the first time.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var newpos = target.global_position + Globals.mOffset
+	set_global_position(newpos)
