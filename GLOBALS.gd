d03b2311a6df3a948a6d29eb1ac49e8dbe29ad90
Tahsin Sarker <36494345+tahsin-s@users.noreply.github.com>
2024-01-20 18:21:59 -0500

    Adjust snap for object scaling

--- GLOBALS.gd
+++ GLOBALS.gd
@@ -15,7 +15,7 @@ const mHeight 	= 22.5
 const mWidth 	= 40.0
 
 const mOffset = Vector3(-m_px32, m_px32, 0)
-const snap = 2 * Vector3(m_px16, m_px16, m_px16);
+const snap = 2 * Vector3(m_px16, m_px16, m_px16) * scale;
 
 func m_to_px16(m_val: float):
 	return m_val * px_m16
