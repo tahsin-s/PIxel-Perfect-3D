ebb2bdea9e15db1295f82d7ce825e24988b10912
Tahsin Sarker <36494345+tahsin-s@users.noreply.github.com>
2024-01-20 14:07:19 -0500

    Remove unused parent

--- CameraPivot.gd
+++ CameraPivot.gd
@@ -2,7 +2,6 @@ extends Marker3D
 
 @export var target: Node3D
 @export var offset: Vector3;
-@onready var parent: Node3D = get_parent()
 # Called when the node enters the scene tree for the first time.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
