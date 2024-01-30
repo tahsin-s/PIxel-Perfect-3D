0b1c240e0999cc374d477fd4894a2d3d168de6aa
Tahsin Sarker <36494345+tahsin-s@users.noreply.github.com>
2024-01-30 10:48:07 -0500

    Disable scaling

--- Scaling_preset.gd
+++ Scaling_preset.gd
@@ -2,11 +2,11 @@ class_name Scaling_preset
 extends Node3D
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	# ensure pixels on xy and xz axes are displayed evenly
-	scale.x = Globals.scale.x
-	scale.y = Globals.scale.y
-	scale.z = Globals.scale.z
-	
-	# offset position to ensure pixel shading is even
-	position += Globals.mOffset
+#func _ready():
+	## ensure pixels on xy and xz axes are displayed evenly
+	#scale.x = Globals.scale.x
+	#scale.y = Globals.scale.y
+	#scale.z = Globals.scale.z
+	#
+	## offset position to ensure pixel shading is even
+	#position += Globals.mOffset
